816dfad69a71b70ae714c5b417da5fc03e766509
David <david.cicconi94@gmail.com>
2023-03-07 06:48:36 -0300

    solving errors 2

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  DB: process.env.DB,
+  DB: process.env.DB || "challengeNode",
   HOST: process.env.HOST || "localhost",
 };
